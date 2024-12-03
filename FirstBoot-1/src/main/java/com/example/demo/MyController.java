d4a5d0a642d147dc36f5a4da14cb604f815b4865
Carleoinserra <inserracarlo@gmail.com>
2024-12-03 14:29:37 +0100

    Add files via upload

---
+++ FirstBoot-1/src/main/java/com/example/demo/MyController.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyController {
+	
+	@GetMapping("/")
+	public String getHello() {
+		
+		return "hello";
+	}
+	
+	@GetMapping("/contatti")
+	public String getHello1() {
+		
+		return "contatti";
+	}
+
+}
