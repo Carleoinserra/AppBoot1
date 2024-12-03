98a7129a26d978c71e012b36cb9aceb95130ac5b
Carleoinserra <inserracarlo@gmail.com>
2024-12-03 14:51:40 +0100

    Add files via upload

--- FirstBoot-1/src/main/java/com/example/demo/MyController.java
+++ FirstBoot-1/src/main/java/com/example/demo/MyController.java
@@ -1,13 +1,21 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MyController {
 	
 	@GetMapping("/")
-	public String getHello() {
+	public String getHello(Model model) {
+		
+		
+		String nome = "Carlo";
+		
+		model.addAttribute("nome", nome);
+		
+		
 		
 		return "hello";
 	}
